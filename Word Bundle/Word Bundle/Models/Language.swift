df86b4f4f98b170e6456fce9757f075535e61ec5
Giga Khizanishvili <gkhizanishvili@adjarabet.com>
2022-07-26 01:11:55 +0400

    Add some languages

--- Word Bundle/Word Bundle/Models/Language.swift
+++ Word Bundle/Word Bundle/Models/Language.swift
@@ -7,4 +7,34 @@
 
 import Foundation
 
-public enum Language { }
+public enum Language: CaseIterable {
+    case armenian
+    case azerbaijani
+    case bengali
+    case czech
+    case dutch
+    case egyptianArabic
+    case english
+    case french
+    case georgian
+    case german
+    case greek
+    case hindi
+    case hungarian
+    case iranianPersian
+    case italian
+    case japanese
+    case kannada
+    case kazakh
+    case korean
+    case mandarinChinese
+    case nepali
+    case polish
+    case portuguese
+    case romanian
+    case spanish
+    case thai
+    case turkish
+    case ukrainian
+    case vietnamese
+}
