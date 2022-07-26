26a06cf1cabb0fc6a5088b48118950ef4c9bd56f
Giga Khizanishvili <gkhizanishvili@adjarabet.com>
2022-07-26 12:05:27 +0400

    Add Language keys and support georgian language localization

--- Word Bundle/Word Bundle/Models/Language.swift
+++ Word Bundle/Word Bundle/Models/Language.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-enum Language: CaseIterable {
+enum Language: String, CaseIterable {
     case armenian
     case azerbaijani
     case bengali
@@ -37,4 +37,40 @@ enum Language: CaseIterable {
     case turkish
     case ukrainian
     case vietnamese
+}
+
+extension Language {
+    var localizableIdentifier: String {
+        switch self {
+        case .armenian: return "en"
+        case .azerbaijani: return "en"
+        case .bengali: return "en"
+        case .czech: return "en"
+        case .dutch: return "en"
+        case .egyptianArabic: return "en"
+        case .english: return "en"
+        case .french: return "en"
+        case .georgian: return "ka"
+        case .german: return "en"
+        case .greek: return "en"
+        case .hindi: return "en"
+        case .hungarian: return "en"
+        case .iranianPersian: return "en"
+        case .italian: return "en"
+        case .japanese: return "en"
+        case .kannada: return "en"
+        case .kazakh: return "en"
+        case .korean: return "en"
+        case .mandarinChinese: return "en"
+        case .nepali: return "en"
+        case .polish: return "en"
+        case .portuguese: return "en"
+        case .romanian: return "en"
+        case .spanish: return "en"
+        case .thai: return "en"
+        case .turkish: return "en"
+        case .ukrainian: return "en"
+        case .vietnamese: return "en"
+        }
+    }
 }
