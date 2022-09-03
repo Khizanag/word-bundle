8d2c450dc2389d1fae202cee22425f1771f46d32
Mariam Ormotsadze <mormo17@freeuni.edu.ge>
2022-09-04 02:45:16 +0400

    add settings page

---
+++ Word Bundle/Word Bundle/Views/SettingsView.swift
@@ -0,0 +1,37 @@
+//
+//  SettingsView.swift
+//  Word Bundle
+//
+//  Created by Mariam Ormotsadze on 03.09.22.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+
+    // TODO: find and fix navigation title bug
+    var body: some View {
+        NavigationView {
+            List {
+                NavigationLink(destination: Text("Reminder Settings")) { // TODO: change destination
+                    Image(systemName: "bell.badge")
+                    Text("Reminder Settings")
+                }
+                .padding()
+                
+                NavigationLink(destination: Text("Change Backgound Theme")) { // TODO: change destination
+                    Image(systemName: "paintbrush")
+                    Text("Change Backgound Theme")
+                }
+                .padding()
+            }
+            .navigationTitle("Settings")
+        }
+    }
+}
+
+struct SettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsView()
+    }
+}
--- Word Bundle/Word Bundle/Views/WelcomeView.swift
+++ Word Bundle/Word Bundle/Views/WelcomeView.swift
@@ -23,10 +23,12 @@ struct WelcomeView: View {
                 Spacer()
                 TextField("Type word to search here", text: $textFieldText)
                     .padding()
+                
                 Button("Fetch Data") {
                     Task {
                         let entry = await dictionariesRepository.entries(of: textFieldText, language: .english)
                         print(entry ?? "Fetched entry was nil")
+                     
                     }
                 }
             }
