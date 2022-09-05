0e9d04ccaaa73b7aca145668030dbf0d32868fa6
Mariam Ormotsadze <mormo17@freeuni.edu.ge>
2022-09-06 00:24:57 +0400

    call images API

--- Word Bundle/Word Bundle/Views/WordView.swift
+++ Word Bundle/Word Bundle/Views/WordView.swift
@@ -9,13 +9,21 @@ import SwiftUI
 
 struct WordView: View {
     @Binding var word: Word
-
+    @State private var imageUrl: String = ""
+    private let imageRepository: ImageRepository = UnsplashImageRepository()
+    
     var body: some View {
-        ZStack {
-            Color.gray
-            Text(word.word)
-                .font(.largeTitle)
-                .foregroundColor(DesignSystem.Color.primaryTextLight()())
+        VStack {
+            //Color.gray
+            Image(uiImage: UIImage.load(url: imageUrl))
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+            
+            Button("Fetch Data") {
+                Task {
+                    imageUrl = await imageRepository.getFullUrl(of: word.word).orEmpty
+                }
+            }
         }
         .navigationTitle(word.word)
         .ignoresSafeArea()
