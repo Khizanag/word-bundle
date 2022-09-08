bbffef9d3c5ff94894d209617b61d0119cfc0bad
Mariam Ormotsadze <mormo17@freeuni.edu.ge>
2022-09-09 01:12:58 +0400

    add delete action sheet on long press on Word Bundle Preview Item

--- Word Bundle/Word Bundle/Views/Word Bundles Views/WordBundlesView.swift
+++ Word Bundle/Word Bundle/Views/Word Bundles Views/WordBundlesView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct WordBundlesView: View {
     @Binding var selection : TabItem
+    @State var isActionSheetPresented = false
     
     var body: some View {
         VStack(spacing: 32) {
@@ -18,8 +19,25 @@ struct WordBundlesView: View {
                     .onTapGesture {
                         selection = .words
                     }
+                    .onLongPressGesture {
+                        isActionSheetPresented = true
+                    }
+                    .actionSheet(isPresented: $isActionSheetPresented) {
+                        let deleteButton = ActionSheet.Button.destructive(Text("Delete")) {
+                            print("Delete button was pressed")
+                            // TODO: Delete Bundle
+                        }
+                        
+                        let cancelButton = ActionSheet.Button.cancel()
+                        
+                        return .init(
+                            title: Text("You are goind to Delete Selected Word Bundle"),
+                            message: Text("Are you sure?"),
+                            buttons: [deleteButton, cancelButton]
+                        )
+                    }
+                    
             }
-
             Spacer()
         }
     }
