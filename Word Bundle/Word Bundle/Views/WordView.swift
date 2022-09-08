91efb60c7635ebe654a71f56e27144e3b77225c3
Giga Khizanishvili <khizanag@gmail.com>
2022-09-08 23:42:06 +0400

    WB-79: Refactor some views
    * Do refactor
    
    * Do cleanup for swiftlint
    
    Co-authored-by: Giga Khizanishvili <gkhizanishvili@adjarabet.com>

--- Word Bundle/Word Bundle/Views/WordView.swift
+++ Word Bundle/Word Bundle/Views/WordView.swift
@@ -30,20 +30,7 @@ struct WordView: View {
                             .font(.system(size: Size.xxxLarge()))
                             .foregroundColor(Color(hex: 0x3F3F3F))
 
-                        Button {
-                            Task { // TODO: make seperate method for this
-                                print("Button was tapped")
-                                guard let audioFile = word.pronunciation.audioFile,
-                                      let url = URL(string: audioFile) else { return }
-                                let playerItem = AVPlayerItem(url: url)
-                                player = AVPlayer(playerItem: playerItem)
-                                await player?.seek(to: .zero)
-                                player?.play()
-                            }
-                        } label: {
-                            Image(systemName: "speaker.wave.3")
-                                .foregroundColor(Color(hex: 0x3F3F3F))
-                        }
+                        soundSpeakerPlayer
                     }
 
                     phoneticSubSection
@@ -54,6 +41,20 @@ struct WordView: View {
             }
         }
         .ignoresSafeArea()
+        .navigationBarTitle(word.word.uppercased())
+    }
+
+    private var soundSpeakerPlayer: some View {
+        Button {
+            Task {
+                await replay()
+            }
+        } label: {
+            Image(systemName: "speaker.wave.3") // TODO: move to DesignSystem
+        }
+        .onAppear {
+            setupPlayer()
+        }
     }
 
     private var sections: some View {
@@ -73,14 +74,14 @@ struct WordView: View {
             senses(for: lexicalEntry)
         }
     }
-    
+
     private var phoneticSubSection: some View {
         HStack {
             if let phoneticSpelling = word.pronunciation.phoneticSpelling {
                 Text(phoneticSpelling)
                     .foregroundColor(Color(hex: 0x3F3F3F))
             }
-            
+
             if let phoneticNotation = word.pronunciation.phoneticNotation {
                 Text("(\(phoneticNotation))")
                     .foregroundColor(Color(hex: 0x3F3F3F))
@@ -121,7 +122,7 @@ struct WordView: View {
     // MARK: - Phrases' Sub Section
     private func phrases(for lexicalEntry: Word.LexicalEntry) -> some View {
         VStack (alignment: .leading) {
-            subSectionHeader(for: "Phrases")
+            subSectionHeader(for: "Phrases") // TODO: Localization
             phrasesRows(for: lexicalEntry)
         }
         .padding(.bottom, Size.xSmall())
@@ -136,7 +137,7 @@ struct WordView: View {
     // MARK: - Examples' Sub Section
     private func examples(for sense: Word.Sense) -> some View {
         VStack (alignment: .leading) {
-            subSectionHeader(for: "Examples")
+            subSectionHeader(for: "Examples") // TODO: Localization
             examplesRows(for: sense)
         }
         .padding(.bottom, Size.xSmall())
@@ -153,7 +154,7 @@ struct WordView: View {
         VStack (alignment: .leading) {
             CollapsibleView(
                 label: {
-                    subSectionHeader(for: "Synonyms")
+                    subSectionHeader(for: "Synonyms") // TODO: Localization
                 },
                 content: {
                     synonymsRows(for: sense)
@@ -175,7 +176,7 @@ struct WordView: View {
         VStack (alignment: .leading) {
              CollapsibleView(
                  label: {
-                     subSectionHeader(for: "Antonyms")
+                     subSectionHeader(for: "Antonyms") // TODO: Localization
                  },
                  content: {
                      antonymsRows(for: sense)
@@ -195,7 +196,7 @@ struct WordView: View {
     // MARK: - Definitions' Sub Section
     private func definitions(for sense: Word.Sense) -> some View {
         VStack (alignment: .leading) {
-            subSectionHeader(for: "Definitions")
+            subSectionHeader(for: "Definitions") // TODO: Localization
             definitionsRows(for: sense)
         }
         .padding(.bottom, Size.xSmall())
@@ -210,7 +211,7 @@ struct WordView: View {
     // MARK: - Short Definitions' Sub Section
     private func shortDefinitions(for sense: Word.Sense) -> some View {
         VStack (alignment: .leading) {
-            subSectionHeader(for: "Short Definitions")
+            subSectionHeader(for: "Short Definitions") // TODO: Localization
             definitionsRows(for: sense)
         }
         .padding(.bottom, Size.xSmall())
@@ -237,6 +238,19 @@ struct WordView: View {
             .font(.system(size: Size.xLarge()))
             .bold()
             .foregroundColor(Color(hex: 0x3F3F3F))
+    }
+
+    private func setupPlayer() {
+        guard let audioFile = word.pronunciation.audioFile,
+              let url = URL(string: audioFile) else { return }
+
+        let playerItem = AVPlayerItem(url: url)
+        player = AVPlayer(playerItem: playerItem)
+    }
+
+    private func replay() async {
+        await player?.seek(to: .zero)
+        await player?.play()
     }
 }
 
--- Word Bundle/Word Bundle/Views/WordsView.swift
+++ Word Bundle/Word Bundle/Views/WordsView.swift
@@ -11,7 +11,6 @@ import SwiftUI
 struct WordsView: View {
     private let dictionariesRepository: DictionariesRepository = OxfordDictionariesRepository()
     private let imageRepository: ImageRepository = UnsplashImageRepository()
-    @State private var player: AVPlayer?
 
     @State private var textFieldText = ""
     @State private var isTextFieldDisabled = false
@@ -24,7 +23,7 @@ struct WordsView: View {
 
     var body: some View {
         VStack {
-            TextField("Type word to search here", text: $textFieldText)
+            TextField("Type word to search here", text: $textFieldText) // TODO: Localization
                 .disabled(isTextFieldDisabled)
                 .padding()
 
@@ -38,7 +37,7 @@ struct WordsView: View {
                 ForEach(entities) { entity in
                     if let word = Word.make(from: entity) {
                         NavigationLink(destination: WordView(word: word)) {
-                            Image(systemName: "wand.and.stars")
+                            Image(systemName: "text.justify.left") // TODO: move to DesignSystem
                             Text(word.word.capitalized)
                         }
                     }
@@ -46,7 +45,7 @@ struct WordsView: View {
                 .onDelete(perform: deleteRow)
             }
         }
-        .navigationTitle("Words")
+        .navigationTitle("Words") // TODO: Localization
     }
 
     private func reset() {
@@ -61,17 +60,9 @@ struct WordsView: View {
                 addWord()
             },
             isLoading: $isButtonLoading,
-            style: .init(
-                width: 300,
-                height: 50,
-                cornerRadius: 8,
-                backgroundColor: DesignSystem.Color.color3().value,
-                loadingColor: .clear,
-                strokeWidth: 3,
-                strokeColor: .indigo
-            ),
-            builder: { // TODO: redesign
-                Text("Add word to Dictionary")
+            style: .init(backgroundColor: DesignSystem.Color.color3().value),
+            builder: {
+                Text("Add word to Dictionary") // TODO: Localization
                     .foregroundColor(.white)
             }
         )
@@ -93,9 +84,7 @@ struct WordsView: View {
                 }
             }
 
-            guard var word = await dictionariesRepository.entries(of: textFieldText, language: .english),
-                  let audioFile = word.pronunciation.audioFile,
-                  let url = URL(string: audioFile) else { return }
+            guard var word = await dictionariesRepository.entries(of: textFieldText, language: .english) else { return }
 
             word.imageUrl = await imageRepository.getFullUrl(of: textFieldText)
 
@@ -110,9 +99,6 @@ struct WordsView: View {
             } catch {
                 fatalError("An error occured during encoding word")
             }
-
-            let playerItem = AVPlayerItem(url: url)
-            player = AVPlayer(playerItem: playerItem)
         }
     }
 
